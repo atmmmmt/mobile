17a136b8c9e76d4bee8c510ed6192425583509d5
Radwan__Sukkari <radwansukkari123@gmail.com>
2024-02-22 12:48:06 +0300

    buildui2

--- lib/features/home_page/bottom_nav_bar/profile/profile_home_page/my_profile/edit_user_profile.dart
+++ lib/features/home_page/bottom_nav_bar/profile/profile_home_page/my_profile/edit_user_profile.dart
@@ -1,5 +1,9 @@
 
+import 'package:atmtha_mobile/core/app_theme/colors.dart';
+import 'package:atmtha_mobile/resources/resources.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class EditUserProfile extends StatelessWidget {
   const EditUserProfile({Key? key}) : super(key: key);
@@ -9,6 +13,112 @@ class EditUserProfile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 10,horizontal: 15),
+        child: Column(
+          children: [
+            Align(
+                alignment: Alignment.topCenter,
+                child: Image.asset(Images.test)),
+            SizedBox(
+              height: 10.h,
+            ),
+            Align(
+              alignment: Alignment.topRight,
+              child: Text(
+                "الاسم",
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+            ),
+        SizedBox(height: 7.h,),
+        Container(
+          height: 68.h,
+          width: 392.w,
+          decoration: BoxDecoration(
+            color: lightColorScheme.onPrimary,
+            border: Border.all(
+                color: lightColorScheme.secondaryContainer,
+                width: 1.5),
+            borderRadius: BorderRadius.circular(15),
+          ),
+          transform: Matrix4.skewX(-.5 / 4),
+          child: Padding(
+            padding: const EdgeInsets.all(13.0),
+            child: Align(
+              alignment: Alignment.centerRight,
+              child:TextFormField(
+                decoration: const InputDecoration(border: InputBorder.none),
+              )
+            ),
+          ),
+        ),
+            SizedBox( height: 13,),
+            Align(
+              alignment: Alignment.topRight,
+              child: Text(
+                "الجنس",
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+            ),
+            SizedBox(height: 7.h,),
+            Container(
+              height: 68.h,
+              width: 392.w,
+              decoration: BoxDecoration(
+                color: lightColorScheme.onPrimary,
+                border: Border.all(
+                    color: lightColorScheme.secondaryContainer,
+                    width: 1.5),
+                borderRadius: BorderRadius.circular(15),
+              ),
+              transform: Matrix4.skewX(-.5 / 4),
+              child: Padding(
+                padding: const EdgeInsets.all(13.0),
+                child: Align(
+                    alignment: Alignment.centerRight,
+                    child:TextFormField(
+                      decoration: const InputDecoration(border: InputBorder.none),
+                    )
+                ),
+              ),
+            ),
+            SizedBox( height: 13,),
+            Align(
+              alignment: Alignment.topRight,
+              child: Text(
+                "البريد الالكتروني4",
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+            ),
+            SizedBox(height: 7.h,),
+            Container(
+              height: 68.h,
+              width: 392.w,
+              decoration: BoxDecoration(
+                color: lightColorScheme.onPrimary,
+                border: Border.all(
+                    color: lightColorScheme.secondaryContainer,
+                    width: 1.5),
+                borderRadius: BorderRadius.circular(15),
+              ),
+              transform: Matrix4.skewX(-.5 / 4),
+              child: Padding(
+                padding: const EdgeInsets.all(13.0),
+                child: Align(
+                    alignment: Alignment.centerRight,
+                    child:TextFormField(
+                      decoration: const InputDecoration(border: InputBorder.none),
+                    )
+                ),
+              ),
+            ),
+
+
+          ],
+        ),
+      ),
+    );
   }
 }
